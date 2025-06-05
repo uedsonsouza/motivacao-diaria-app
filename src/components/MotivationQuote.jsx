868c9685d66b525f6426e7d0627acfbb4b1a3530
uedsonsouza <gaiek2016rmc@gmail.com>
2025-06-04 23:47:04 -0300

    setup initial

---
+++ src/components/MotivationQuote.jsx
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  position: fixed;
+  top: 16px;
+  right: 16px;
+  z-index: 1000;
+`;
+const QuoteText = styled.p`
+  font-size: 1.5em;
+  color: #333;
+  margin: 20px;
+  text-align: center;
+  font-family: cursive, sans-serif;
+`;
+
+const QuoteAuthor = styled.p`
+  font-size: 1em;
+  color: #555;
+  margin-top: 10px;
+  text-align: center;
+  font-family: fantasy, papyrus;
+`;
+
+const QuoteContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2em;
+  margin: 0 20px 0 20px;
+  color: #333;
+  min-height: 100vh;
+`;
+
+const QuoteContainerError = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: red;
+  font-size: 1.2em;
+  margin: 20px;
+`;
+
+const QuoteContainerLoading = styled.p`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.2em;
+  margin: 20px;
+  color: #333;
+`;
+
+const MotivationQuote = () => {
+  const [quote, setQuote] = useState({ text: "", author: "" });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "https://api.realinspire.live/v1/quotes/random"
+        );
+        if (!response.ok) {
+          throw new Error("Http error! Status: " + response.status);
+        }
+        const data = await response.json();
+        const randomIndex = Math.floor(Math.random() * data.length);
+        setQuote({
+          text: data[randomIndex].content,
+          author: data[randomIndex].author,
+        });
+        setLoading(false);
+      } catch (err) {
+        console.error("Error fetching quote:", err);
+        setError("Error fetching quote. Please try again later.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchQuote();
+  }, []);
+
+  if (loading) {
+    return <QuoteContainerLoading>Loading...</QuoteContainerLoading>;
+  }
+  if (error) {
+    return <QuoteContainerError>{error}</QuoteContainerError>;
+  }
+
+  return (
+    <>
+      <HeaderContainer>
+        <a
+          href="https://github.com/uedsonsouza"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://img.shields.io/badge/Gaiek-000000?style=flat&logo=github&logoColor=white"
+            alt="GitHub Profile"
+          />
+        </a>
+      </HeaderContainer>
+
+      <QuoteContainer>
+        <QuoteText>{quote.text}</QuoteText>
+        <QuoteAuthor>{quote.author}</QuoteAuthor>
+      </QuoteContainer>
+    </>
+  );
+};
+
+export default MotivationQuote;
